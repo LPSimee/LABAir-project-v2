4acfe1e841483ba5392a5091e6d2b92e75ad9cf6
LP Simeon <lp.simeon@yahoo.com>
2025-10-15 22:04:00 +0200

    feat(header): applied blur on hover event of link-voce-header selectors

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  standalone: false,
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: false,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+    isBlurActive: boolean = false;
+    applyBlurBackground() {
+        this.isBlurActive = true;
+    }
+
+    removeBlurBackground() {
+        this.isBlurActive = false;
+    }
 }
