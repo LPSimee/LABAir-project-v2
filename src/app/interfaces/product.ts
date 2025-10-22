ef0f2d2282e0d3045ff0afcff01307f79fd745d5
LP Simeon <lp.simeon@yahoo.com>
2025-10-22 18:55:42 +0200

    feat(Product): created Product Interface

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number;
+    nome: string;
+    categoria: string;
+    prezzo: number;
+    immagineUrl: string;
+    nuovo_arrivo: boolean;
+    best_seller: number;
+}
