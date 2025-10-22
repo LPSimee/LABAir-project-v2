582bff32544883caa4a76c94f49fc666b67b409a
LP Simeon <lp.simeon@yahoo.com>
2025-10-22 13:53:49 +0200

    feat(home): implemented scroll function in order to scroll horizontally for the sliders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  standalone: false,
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: false,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements AfterViewInit {
+    // All references of  <ul> with #widgetContent
+    @ViewChildren('widgetContent') sliderContainers!: QueryList<ElementRef>;
+
+    ngAfterViewInit() {
+        console.log('N. sliders:', this.sliderContainers.length);
+    }
+
+    scroll(sliderIndex: number, direction: 'left' | 'right') {
+
+        // In order to access the correct slider, we use the index
+        const sliderRef = this.sliderContainers.get(sliderIndex);
+
+        if (sliderRef) {
+            const element = sliderRef.nativeElement;
+
+            const scrollStep = element.offsetWidth;
+
+            const amount = direction === 'right' ? scrollStep : -scrollStep;
+
+            console.log(`Scrolling slider ${sliderIndex} by ${amount}px. Current width: ${scrollStep}`);
+
+            element.scrollBy({
+                left: amount,
+                behavior: 'smooth'
+            });
+        } else {
+            console.error(`Slider all'indice ${sliderIndex} non trovato.`);
+        }
+    }
 }
