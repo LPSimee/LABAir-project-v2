e95cc5152a5e4b9469f8615c622bcd30cc73c268
LP Simeon <lp.simeon@yahoo.com>
2026-01-02 23:58:00 +0100

    feat(product-details): created functioning button slider for the thumbnail gallery
    created selectedIndex, 'goToNextImg' and 'goToPreviousImg' methods and modifed selectThumbnailImage method to add data binding of selectedIndex
    
    removed this. term in html of product-detail and shoe-list components

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -15,7 +15,8 @@ export class ProductDetailsComponent {
 
     selectedProduct: Product;
     selectedColorway: string = ""; // From the colorParam in the Url
-    selectedCwImgs: string[] = []; // Cw = colorway, Imgs = Images
+    selectedCwImgs: string[]; // Cw = colorway, Imgs = Images
+    selectedIndex: number = 0;
 
     // defaultImage: string = ""; substituted with currentDisplayImage
     defaultColorway: string = "nero";
@@ -23,6 +24,7 @@ export class ProductDetailsComponent {
     currentDisplayImage: string = ""; // The image shown in "contenitore-immagine-scarpa" (big image)
 
     ngOnInit(): void {
+        console.log("selectedIndex=" + this.selectedIndex);
         // substituted the snapshot with the subscribe method of 'paramMap'
         this.route.paramMap.subscribe(params => {
             const nameParam = params.get('slug');
@@ -45,10 +47,21 @@ export class ProductDetailsComponent {
         });
     }
 
+    // showShoeSize(): void {
+    //     console.log("currentDisplayImage= " + this.currentDisplayImage);
+    //     console.log("Immagini: ", this.selectedCwImgs);
+    //     let activeIndex = this.selectedCwImgs.indexOf(this.currentDisplayImage);
+    //     // console.log(this.selectedCwImgs.indexOf(currentImage));
+    //     console.log("activeIndex=", activeIndex)
 
-    showShoeSize(): void {
-        console.log("Ciao");
-    }
+    //     if (activeIndex >= this.selectedCwImgs.length - 1) {
+    //         activeIndex = 0;
+    //         this.currentDisplayImage = this.selectedCwImgs[activeIndex];
+    //     } else
+    //         this.currentDisplayImage = this.selectedCwImgs[activeIndex + 1];
+
+    //     console.log("currentDisplayImage=", this.currentDisplayImage);
+    // }
 
     // Flag to check whether the page header is sticky or not
     isImageGallerySticky: boolean = false;
@@ -64,10 +77,34 @@ export class ProductDetailsComponent {
     }
 
     // Method used to show the image in the main container of the hovered thumbnail
-    selectThumbnailImage(nuovoSrc: string) {
+    selectThumbnailImage(nuovoSrc: string, x: number) {
         // If the image is the one dispalyed, then do nothing
         if (this.currentDisplayImage === nuovoSrc) return;
-        this.currentDisplayImage = nuovoSrc; // to subsitute the current image with the one hovered
+        this.currentDisplayImage = nuovoSrc; // to update the current image with the one hovered
+        this.selectedIndex = x;
+    }
+
+    // Methods used for the thumbnail slider
+    goToNextImg(): void {
+        this.selectedIndex++;
+        if (this.selectedIndex > this.selectedCwImgs.length - 1) {
+            this.selectedIndex = 0;
+        }
+
+        this.currentDisplayImage = this.selectedCwImgs[this.selectedIndex];
+
+        // console.log("selectedIndex=" + this.selectedIndex);
+    }
+
+    goToPreviousImg(): void {
+        this.selectedIndex--;
+        if (this.selectedIndex < 0) {
+            this.selectedIndex = this.selectedCwImgs.length - 1;
+        }
+
+        this.currentDisplayImage = this.selectedCwImgs[this.selectedIndex];
+
+        // console.log("selectedIndex=" + this.selectedIndex);
     }
 
     // Method get the slug with the dashes instead of the spaces
